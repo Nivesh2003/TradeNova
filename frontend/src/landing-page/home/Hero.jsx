0ff1ab938d534ee7d900345e2f876da11fc1eb74
Nivesh2003 <nivesh.ch30@gmail.com>
2025-08-25 19:48:41 +0530

    Adding home page section1

--- frontend/src/landing-page/home/Hero.jsx
+++ frontend/src/landing-page/home/Hero.jsx
@@ -5,7 +5,7 @@ function Hero() {
     <div className="container p-5">
       {/* Hero section */}
       <div className="row text-center">
-        <img src="public/images/tradeNova.png" alt="hero-image" className='img-fluid mb-5' height={'650px'} width={'800px'}/>
+        <img src="public/images/homeHero.png" alt="hero-image" className='img-fluid mb-5' height={'650px'} width={'800px'}/>
         <h1 className='mt-5'>Ready to Nova-fy Your Trades?</h1>
         <p>Experience a platform designed for modern traders.</p>
         <button className="btn btn-sm btn-green fs-5 rounded p-2 mx-auto">Create an account</button>
