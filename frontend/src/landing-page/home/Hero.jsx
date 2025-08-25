b674e3c71ced7f6f1e0914b194a9769215284a4c
Nivesh2003 <nivesh.ch30@gmail.com>
2025-08-25 19:36:41 +0530

    Added content in home page

--- frontend/src/landing-page/home/Hero.jsx
+++ frontend/src/landing-page/home/Hero.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
-
+import React from 'react';
+import '../../index.css';
 function Hero() {
   return (
     <div className="container p-5">
+      {/* Hero section */}
       <div className="row text-center">
-        <img src="./public/images/tradeNova.png" alt="hero-image" className='mb-5' height={'650px'} width={'800px'}/>
+        <img src="public/images/tradeNova.png" alt="hero-image" className='img-fluid mb-5' height={'650px'} width={'800px'}/>
         <h1 className='mt-5'>Ready to Nova-fy Your Trades?</h1>
         <p>Experience a platform designed for modern traders.</p>
-        <button className="rounded">Create an account</button>
+        <button className="btn btn-sm btn-green fs-5 rounded p-2 mx-auto">Create an account</button>
+      </div> 
+      {/* Hero end */}
+      {/* Award start */}
+      <div className="row">
+        <div className="col-6">
+          <img src="public/images/award.png" className="img-fluid mt-5 mb-5" alt="" />
+        </div>
+        <div className="col-6"></div>
       </div>
+      {/* Award end */}
     </div>
   )
 }
