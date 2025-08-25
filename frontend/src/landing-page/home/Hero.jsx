8e5e595a108c3f0e123416819f1c5eeeceb95b66
Nivesh2003 <nivesh.ch30@gmail.com>
2025-08-25 18:51:55 +0530

    Adding home page section1

--- frontend/src/landing-page/home/Hero.jsx
+++ frontend/src/landing-page/home/Hero.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 function Hero() {
   return (
-    <div>Hero</div>
+    <div className="container p-5">
+      <div className="row text-center">
+        <img src="./public/images/tradeNova.png" alt="hero-image" className='mb-5' height={'650px'} width={'800px'}/>
+        <h1 className='mt-5'>Ready to Nova-fy Your Trades?</h1>
+        <p>Experience a platform designed for modern traders.</p>
+        <button className="rounded">Create an account</button>
+      </div>
+    </div>
   )
 }
 
