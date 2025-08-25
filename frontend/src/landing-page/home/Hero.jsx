82b06fe7e5737f0c2b1e85b6165471aebabec7c7
Nivesh2003 <nivesh.ch30@gmail.com>
2025-08-25 22:49:40 +0530

    Adding more content

--- frontend/src/landing-page/home/Hero.jsx
+++ frontend/src/landing-page/home/Hero.jsx
@@ -1,28 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import '../../index.css';
 
 function Hero() {
+  useEffect(() => {
+    AOS.init({ duration: 1000, once: false, mirror: true });
+  }, []);
+
   return (
     <div className="container p-5">
       {/* Hero section */}
       <div className="row text-center">
-        <img src="images/homeHero.png" alt="hero-image" className='hero-img mb-4'/>
-        <h1 className='mt-4'>Ready to Nova-fy Your Trades?</h1>
-        <p>Experience a platform designed for modern traders.</p>
-        <button className="btn btn-sm btn-green fs-5 rounded p-2 mx-auto">Create an account</button>
+        <img data-aos="fade-up" src="/images/homeHero.png" alt="hero-image" className='hero-img mb-4'/>
+        <h1 data-aos="fade-up" className='mt-4'>Ready to Nova-fy Your Trades?</h1>
+        <p data-aos="fade-up">Experience a platform designed for modern traders.</p>
+        <button data-aos="fade-up" className="btn btn-sm btn-green fs-5 rounded p-2 mx-auto">Create an account</button>
       </div> 
-      {/* Hero end */}
 
-      {/* Award start */}
+      {/* Award section */}
       <div className="row mt-5 align-items-center">
-        <div className="col-md-6 mb-4 mb-md-0">
-          <img src="images/award.png" className="img-fluid" alt="Award" />
+        <div className="col-md-6 mb-4 mb-md-0" data-aos="fade-up">
+          <img src="/images/award.png" className="img-fluid" alt="Award" />
         </div>
-        <div className="col-md-6">
+        <div className="col-md-6" data-aos="fade-up">
           <h2 className='mb-3' style={{fontSize:'30px'}}>Award-Winning Trading Experience</h2>
-          <p className='mb-4'>
-            Recognized for innovation and trust, TradeNova brings you a platform where smart technology meets seamless trading. Join thousands of traders who are already experiencing a new era of investments.
-          </p>
+          <p className='mb-4'>Recognized for innovation and trust, TradeNova brings you a platform where smart technology meets seamless trading.</p>
           <div className="row">
             <div className="col-6">
               <ul>
@@ -37,12 +40,11 @@ function Hero() {
               </ul>
             </div>
           </div>
-          <div className="mt-3">
-            <img src="images/pressLogos.png" alt="Press Logos" className="img-fluid" />
+          <div className="mt-3" data-aos="fade-up">
+            <img src="/images/pressLogos.png" alt="Press Logos" className="img-fluid" />
           </div>
         </div>
       </div>
-      {/* Award end */}
     </div>
   )
 }
--- frontend/src/landing-page/home/Stats.jsx
+++ frontend/src/landing-page/home/Stats.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function Stats() {
   return (
-    <div>Stats</div>
+    <div className="container">
+      <div className="row">
+        <div className="col-6">
+
+        </div>
+        <div className="col-6">
+          <img src="images/" alt="" />
+        </div>
+      </div>
+    </div>
   )
 }
 
