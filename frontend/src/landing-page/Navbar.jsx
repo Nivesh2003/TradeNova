e4c34608c6eb65ce0fdbde479e9d31d119f62bf7
Nivesh2003 <nivesh.ch30@gmail.com>
2025-08-27 00:04:32 +0530

    About us page hero

--- frontend/src/landing-page/Navbar.jsx
+++ frontend/src/landing-page/Navbar.jsx
@@ -1,40 +1,53 @@
-import React from 'react'
-import '../index.css';
+import React from 'react';
 import { Link } from 'react-router-dom';
+import '../index.css';
+
 function Navbar() {
   return (
-<nav className="navbar navbar-expand-lg p-0 border-bottom" style={{backgroundColor:'white'}}>
-  <div className="container">  
-    <Link className="navbar-brand" to="/">
-      <img 
-        src="/images/logoTN.svg" 
-        alt="" 
-      />
-    </Link>
-    <button 
-      className="navbar-toggler" 
-      type="button" 
-      data-bs-toggle="collapse" 
-      data-bs-target="#navbarSupportedContent" 
-      aria-controls="navbarSupportedContent" 
-      aria-expanded="false" 
-      aria-label="Toggle navigation"
-    >
-      <span className="navbar-toggler-icon"></span>
-    </button>
+    <nav className="navbar navbar-expand-lg p-0 border-bottom" style={{ backgroundColor: 'white' }}>
+      <div className="container d-flex align-items-center">
+        {/* Logo */}
+        <Link className="navbar-brand" to="/">
+          <img
+            src="/images/logoTN.svg"
+            alt="TradeNova Logo"
+            className="navbar-logo"
+          />
+        </Link>
 
-    <div className="collapse navbar-collapse d-flex justify-content-end" id="navbarSupportedContent">
-      <ul className="navbar-nav mb-2 mb-lg-0">
-        <li className="nav-item"><Link to='/about' className="nav-link active">About</Link></li>
-        <li className="nav-item"><Link className="nav-link active" to='/notfound'>Product</Link></li>
-        <li className="nav-item"><Link className="nav-link active" to="/support">Support</Link></li>
-        <li className="nav-item"><Link className="nav-link active" to='/signup'>Signup</Link></li>
-      </ul>
-    </div>
-  </div>
-</nav>
+        {/* Toggler */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-  )
+        {/* Nav links */}
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link to='/about' className="nav-link active">About</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link active" to='/notfound'>Product</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link active" to="/support">Support</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link active" to='/signup'>Signup</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
--- frontend/src/landing-page/about/AboutPage.jsx
+++ frontend/src/landing-page/about/AboutPage.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
-
+import '../../index.css'
 function AboutPage() {
   return (
-    <div>AboutPage coming soon</div>
+    <>
+  <div className="container-fluid">
+      <div className="row">
+      <div className="top-section">
+      {/* Coins rolling animation using CSS circles */}
+
+
+      {/* Heading */}
+      <h1 className="top-heading">
+        Trade Smarter, Not Harder <br />
+        <div className="coin coin2"></div>
+        Welcome to Your Modern Trading Playground
+      </h1>
+
+      {/* Optional tagline */}
+      <p className="top-subtext">
+        Innovative tools, seamless experience, and insights at your fingertips.
+      </p> 
+      <div className="coin coin3"></div>
+      </div>
+      <div className="coin coin1" style={{marginTop:'300px'}}></div>
+    </div> {/* Top section ends*/}
+
+    
+
+  </div>
+    </>
   )
 }
 
