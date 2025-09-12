8d8c3ec0a15210ee11601a7e97e98be0e4ca7de8
Nivesh2003 <nivesh.ch30@gmail.com>
2025-09-13 01:14:12 +0530

    Created connection to mongo cloud

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,24 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+require('dotenv').config();
+
+
+const port = process.env.PORT || 5000
+const url = process.env.MONGO_URL
+
 
 const app = express();
 
-app.listen(5000,()=>{
-    console.log("Server is started");
-})
+app.listen(port, () => {
+    console.log(`Server is started at ${port}`);
+    
+    mongoose.connect(url)
+        .then(() => {
+            console.log("MongoDB connected 😊");
+        })
+        .catch((e) => {
+            console.log("MongoDB connection error:", e);
+        });
+});
+
